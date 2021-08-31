259ecf6f470532946c0ee31dd58ad145a753beab
Lucas de Azevedo Godoi Lomba <lucasazevedolomba@gmail.com>
2021-08-30 21:36:59 -0300

    Conexao com a api de pagamentos

--- ignews/src/components/SignInButton/index.tsx
+++ ignews/src/components/SignInButton/index.tsx
@@ -1,18 +1,20 @@
-import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut ,useSession } from 'next-auth/client'
+
+import styles from './styles.module.scss'
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton} type="button">
+    return session ? (
+        <button className={styles.signInButton} type="button" onClick={() => signOut()}>
             <FaGithub color="#04D361"/>
-            Lucas Lomba
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
     ) : (
-        <button className={styles.signInButton} type="button">
+        <button className={styles.signInButton} type="button" onClick={() => signIn('github')}>
             <FaGithub color="#EBA417"/>
             Sign in with GitHub
         </button>
--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api';
+import { getSripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps{
@@ -5,8 +8,29 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
+    const [session] = useSession()
+    
+    async function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return;
+        }
+
+        try{
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getSripeJs()
+
+            await stripe.redirectToCheckout({sessionId: sessionId})
+        } catch (err) {
+            alert(err.message)
+        }
+    }
+
     return(
-        <button type="button" className={styles.subscribeButton}>
+        <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
            Subscribe now
         </button>
     )
